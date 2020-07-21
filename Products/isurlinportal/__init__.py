8ca75150916e7c3b1c2bbfd22e3ecddb4197e000
Maurits van Rees <maurits@vanrees.org>
2020-07-21 11:31:04 +0200

    Keep only the isURLInPortal function, and related parts.

--- Products/isurlinportal/__init__.py
+++ Products/isurlinportal/__init__.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
-from AccessControl import ClassSecurityInfo
-from Acquisition import aq_parent, aq_inner
-from AccessControl.class_init import InitializeClass
 from plone.registry.interfaces import IRegistry
 from posixpath import normpath
-from Products.CMFCore.interfaces import ISiteRoot
-from Products.CMFCore.URLTool import URLTool as BaseTool
 from Products.CMFPlone.interfaces import ILoginSchema
-from Products.CMFPlone.PloneBaseTool import PloneBaseTool
-from Products.CMFPlone.patches.gtbn import rewrap_in_request_container
 from six.moves.html_parser import HTMLParser
 from six.moves.urllib import parse
 from zope.component import getUtility
+
+# This is the class we will patch:
+from Products.CMFPlone.URLTool import URLTool
 
 import html
 import re
@@ -96,99 +92,83 @@ def safe_url_first_char(url):
     return True
 
 
-class URLTool(PloneBaseTool, BaseTool):
+def isURLInPortal(self, url, context=None):
+    # Check if a given url is on the same host and contains the portal
+    # path.  Used to ensure that login forms can determine relevant
+    # referrers (i.e. in portal).  Also return true for some relative
+    # urls if context is passed in to allow for url parsing. When context
+    # is not provided, assume that relative urls are in the portal. It is
+    # assumed that http://portal is the same portal as https://portal.
 
-    meta_type = "Plone URL Tool"
-    security = ClassSecurityInfo()
-    toolicon = "skins/plone_images/link_icon.png"
+    # External sites listed in 'allow_external_login_sites' of
+    # site_properties are also considered within the portal to allow for
+    # single sign on.
 
-    security.declarePublic("isURLInPortal")
+    if url and not safe_url_first_char(url):
+        return False
 
-    def isURLInPortal(self, url, context=None):
-        # Check if a given url is on the same host and contains the portal
-        # path.  Used to ensure that login forms can determine relevant
-        # referrers (i.e. in portal).  Also return true for some relative
-        # urls if context is passed in to allow for url parsing. When context
-        # is not provided, assume that relative urls are in the portal. It is
-        # assumed that http://portal is the same portal as https://portal.
-
-        # External sites listed in 'allow_external_login_sites' of
-        # site_properties are also considered within the portal to allow for
-        # single sign on.
-
-        if url and not safe_url_first_char(url):
+    # sanitize url
+    url = re.sub("^[\x00-\x20]+", "", url).strip()
+    cmp_url = url.lower()
+    for bad in BAD_URL_PARTS:
+        if bad in cmp_url:
             return False
 
-        # sanitize url
-        url = re.sub("^[\x00-\x20]+", "", url).strip()
-        cmp_url = url.lower()
-        for bad in BAD_URL_PARTS:
-            if bad in cmp_url:
-                return False
+    p_url = self()
 
-        p_url = self()
+    schema, u_host, u_path, _, _, _ = parse.urlparse(url)
+    if schema and schema not in ALLOWED_SCHEMAS:
+        # Redirecting to 'data:' may be harmful,
+        # and redirecting to 'mailto:' or 'ftp:' is silly.
+        return False
 
-        schema, u_host, u_path, _, _, _ = parse.urlparse(url)
-        if schema and schema not in ALLOWED_SCHEMAS:
-            # Redirecting to 'data:' may be harmful,
-            # and redirecting to 'mailto:' or 'ftp:' is silly.
+    # Someone may be doing tricks with escaped html code.
+    if six.PY2:
+        unescaped_url = hp.unescape(url)
+    else:
+        unescaped_url = html.unescape(url)
+    if unescaped_url != url:
+        if not self.isURLInPortal(unescaped_url):
             return False
 
-        # Someone may be doing tricks with escaped html code.
-        if six.PY2:
-            unescaped_url = hp.unescape(url)
+    if not u_host and not u_path.startswith("/"):
+        if context is None:
+            return True  # old behavior
+        if not context.isPrincipiaFolderish:
+            useurl = context.aq_parent.absolute_url()
         else:
-            unescaped_url = html.unescape(url)
-        if unescaped_url != url:
-            if not self.isURLInPortal(unescaped_url):
-                return False
+            useurl = context.absolute_url()
+    else:
+        useurl = p_url  # when u_path.startswith('/')
+    if not useurl.endswith("/"):
+        useurl += "/"
 
-        if not u_host and not u_path.startswith("/"):
-            if context is None:
-                return True  # old behavior
-            if not context.isPrincipiaFolderish:
-                useurl = context.aq_parent.absolute_url()
-            else:
-                useurl = context.absolute_url()
-        else:
-            useurl = p_url  # when u_path.startswith('/')
-        if not useurl.endswith("/"):
-            useurl += "/"
+    # urljoin to current url to get an absolute path
+    _, u_host, u_path, _, _, _ = parse.urlparse(parse.urljoin(useurl, url))
 
-        # urljoin to current url to get an absolute path
-        _, u_host, u_path, _, _, _ = parse.urlparse(parse.urljoin(useurl, url))
+    # normalise to end with a '/' so /foobar is not considered within /foo
+    if not u_path:
+        u_path = "/"
+    else:
+        u_path = normpath(u_path)
+        if not u_path.endswith("/"):
+            u_path += "/"
+    _, host, path, _, _, _ = parse.urlparse(p_url)
+    if not path.endswith("/"):
+        path += "/"
+    if host == u_host and u_path.startswith(path):
+        return True
 
-        # normalise to end with a '/' so /foobar is not considered within /foo
-        if not u_path:
-            u_path = "/"
-        else:
-            u_path = normpath(u_path)
-            if not u_path.endswith("/"):
-                u_path += "/"
-        _, host, path, _, _, _ = parse.urlparse(p_url)
+    registry = getUtility(IRegistry)
+    settings = registry.forInterface(ILoginSchema, prefix="plone")
+    for external_site in settings.allow_external_login_sites:
+        _, host, path, _, _, _ = parse.urlparse(external_site)
         if not path.endswith("/"):
             path += "/"
         if host == u_host and u_path.startswith(path):
             return True
-
-        registry = getUtility(IRegistry)
-        settings = registry.forInterface(ILoginSchema, prefix="plone")
-        for external_site in settings.allow_external_login_sites:
-            _, host, path, _, _, _ = parse.urlparse(external_site)
-            if not path.endswith("/"):
-                path += "/"
-            if host == u_host and u_path.startswith(path):
-                return True
-        return False
-
-    def getPortalObject(self):
-        portal = aq_parent(aq_inner(self))
-        if portal is None:
-            portal = getUtility(ISiteRoot)
-        # Make sure portal can acquire REQUEST
-        return rewrap_in_request_container(portal, context=self)
+    return False
 
 
-URLTool.__doc__ = BaseTool.__doc__
-
-InitializeClass(URLTool)
+# Add our method to the URLTool.
+URLTool.isURLInPortal = isURLInPortal
