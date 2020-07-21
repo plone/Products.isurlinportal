6cdc1dbe0d2ea480d001861eda59f7859f535f24
Maurits van Rees <maurits@vanrees.org>
2020-07-21 11:20:26 +0200

    black

--- Products/isurlinportal/__init__.py
+++ Products/isurlinportal/__init__.py
@@ -25,17 +25,17 @@ hp = HTMLParser()
 # A mailto schema is an obvious sign of a url that is not in the portal.
 # This is a whitelist.
 ALLOWED_SCHEMAS = [
-    'https',
-    'http',
+    "https",
+    "http",
 ]
 # These bad parts are not allowed in urls that are in the portal:
 # This is a blacklist.
 BAD_URL_PARTS = [
-    '\\\\',
-    '<script',
-    '%3cscript',
-    'javascript:',
-    'javascript%3a',
+    "\\\\",
+    "<script",
+    "%3cscript",
+    "javascript:",
+    "javascript%3a",
 ]
 
 # Determine allowed ascii characters.
@@ -47,6 +47,7 @@ BAD_URL_PARTS = [
 # - @ because we have views starting with @@
 # - + because we have ++resource++ urls
 allowed_ascii = string.ascii_letters + string.digits + "./@+"
+
 
 def safe_url_first_char(url):
     # For character code points higher than 127, the bytes representation of a character
@@ -97,11 +98,11 @@ def safe_url_first_char(url):
 
 class URLTool(PloneBaseTool, BaseTool):
 
-    meta_type = 'Plone URL Tool'
+    meta_type = "Plone URL Tool"
     security = ClassSecurityInfo()
-    toolicon = 'skins/plone_images/link_icon.png'
+    toolicon = "skins/plone_images/link_icon.png"
 
-    security.declarePublic('isURLInPortal')
+    security.declarePublic("isURLInPortal")
 
     def isURLInPortal(self, url, context=None):
         # Check if a given url is on the same host and contains the portal
@@ -119,7 +120,7 @@ class URLTool(PloneBaseTool, BaseTool):
             return False
 
         # sanitize url
-        url = re.sub('^[\x00-\x20]+', '', url).strip()
+        url = re.sub("^[\x00-\x20]+", "", url).strip()
         cmp_url = url.lower()
         for bad in BAD_URL_PARTS:
             if bad in cmp_url:
@@ -142,7 +143,7 @@ class URLTool(PloneBaseTool, BaseTool):
             if not self.isURLInPortal(unescaped_url):
                 return False
 
-        if not u_host and not u_path.startswith('/'):
+        if not u_host and not u_path.startswith("/"):
             if context is None:
                 return True  # old behavior
             if not context.isPrincipiaFolderish:
@@ -151,31 +152,31 @@ class URLTool(PloneBaseTool, BaseTool):
                 useurl = context.absolute_url()
         else:
             useurl = p_url  # when u_path.startswith('/')
-        if not useurl.endswith('/'):
-            useurl += '/'
+        if not useurl.endswith("/"):
+            useurl += "/"
 
         # urljoin to current url to get an absolute path
         _, u_host, u_path, _, _, _ = parse.urlparse(parse.urljoin(useurl, url))
 
         # normalise to end with a '/' so /foobar is not considered within /foo
         if not u_path:
-            u_path = '/'
+            u_path = "/"
         else:
             u_path = normpath(u_path)
-            if not u_path.endswith('/'):
-                u_path += '/'
+            if not u_path.endswith("/"):
+                u_path += "/"
         _, host, path, _, _, _ = parse.urlparse(p_url)
-        if not path.endswith('/'):
-            path += '/'
+        if not path.endswith("/"):
+            path += "/"
         if host == u_host and u_path.startswith(path):
             return True
 
         registry = getUtility(IRegistry)
-        settings = registry.forInterface(ILoginSchema, prefix='plone')
+        settings = registry.forInterface(ILoginSchema, prefix="plone")
         for external_site in settings.allow_external_login_sites:
             _, host, path, _, _, _ = parse.urlparse(external_site)
-            if not path.endswith('/'):
-                path += '/'
+            if not path.endswith("/"):
+                path += "/"
             if host == u_host and u_path.startswith(path):
                 return True
         return False
