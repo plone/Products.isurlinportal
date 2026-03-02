95877035f28498c969dcf9b9e32851243f60796d
Maurits van Rees <maurits@vanrees.org>
2026-03-02 14:32:21 +0100

    Back to development: 4.0.1
    [ci skip]

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="Products.isurlinportal",
-    version="4.0.0",
+    version="4.0.1.dev0",
     description="Implementation of isURLInPortal method in Plone",
     long_description=(
         f"{Path('README.rst').read_text()}\n{Path('CHANGES.rst').read_text()}\n"
